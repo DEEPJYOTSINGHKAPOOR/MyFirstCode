bf8a602af6e0e7ed91a0d433c4d05a46fac3aac1
Deepjyot Singh Kapoor <45242325+DEEPJYOTSINGHKAPOOR@users.noreply.github.com>
2019-04-23 19:18:21 +0530

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,5 +2,8 @@
 #include<conio.h>
 void main(){
   printf("Hello Wolrd !");
+  printf("Repository Name: MyFirstCode\n");
+  printf("Branch:Master \n");
+  printf("Last Commit: 7:18.23/04/2019.\n");
 
 }
